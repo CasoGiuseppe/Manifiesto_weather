566916d4421b0a31c39d8e2cabd765b116d0790d
gcaso <76.giuseppe.caso@gmail.com>
2022-11-15 11:24:34 +0100

    add intro message for dashboard view

--- src/app/ui/views/dashboard/Dashboard.vue
+++ src/app/ui/views/dashboard/Dashboard.vue
@@ -1,3 +1,8 @@
 <template>
-  dashboard
+  <section class="dashboard">
+    <article class="dashboard--is-no-logged">
+      <p class="dashboard__highlight">Welcome to our Weather dashboard</p>
+    </article>
+  </section>
 </template>
+<style lang="scss" src="./Dashboard.scss" />
