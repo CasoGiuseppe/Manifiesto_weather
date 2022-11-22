d38616a753a1cd0a1d066c765614e79c4feb4a01
gcaso <76.giuseppe.caso@gmail.com>
2022-11-22 14:34:38 +0100

    handle charts visible state on usr login and data charge

--- src/app/ui/views/dashboard/Dashboard.vue
+++ src/app/ui/views/dashboard/Dashboard.vue
@@ -1,14 +1,23 @@
 <template>
   <section class="dashboard">
-    <article class="dashboard--is-no-logged">
-      <p class="dashboard__highlight">
-        Welcome to our Weather dashboard
-        <RouterLink :to="{name: 'sign'}" class="link link--is-simple">Sign in </RouterLink>
-      </p>
-    </article>
-    <ul class="dashboard__charts">
+    <Transition
+        mode="out-in"
+        name="appear-extra-panel"
+        appear
+      >
+      <ul
+        class="dashboard__charts"
+        v-if="isPageReady"
+      >
         <li>
-          <BaseBadge id="row" :is="[IsBadge.ROW]">
+          <BaseBadge id="row" :is="[IsBadge.COLUMN]">
+            <template #title>title</template>
+            <template #payoff>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et </template>
+            <template #content><ChartBarIcon style="fill: white" /></template>
+          </BaseBadge>
+        </li>
+        <li>
+          <BaseBadge id="row" :is="[IsBadge.COLUMN]">
             <template #title>title</template>
             <template #payoff>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et </template>
             <template #content><ChartBarIcon style="fill: white" /></template>
@@ -22,14 +31,7 @@
           </BaseBadge>
         </li>
         <li>
-          <BaseBadge id="row" :is="[IsBadge.ROW]">
-            <template #title>title</template>
-            <template #payoff>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et </template>
-            <template #content><ChartBarIcon style="fill: white" /></template>
-          </BaseBadge>
-        </li>
-        <li>
-          <BaseBadge id="row" :is="[IsBadge.ROW]">
+          <BaseBadge id="row" :is="[IsBadge.COLUMN]">
             <template #title>title</template>
             <template #payoff>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et </template>
             <template #content><ChartBarIcon style="fill: white" /></template>
@@ -37,11 +39,39 @@
         </li>
         <li class="dashboard__charts--is-expand">5</li>
       </ul>
+
+      <!-- log out user-->
+      <article
+        v-else
+        class="dashboard--is-no-logged"
+      >
+        <p class="dashboard__highlight">
+          Welcome to our Weather dashboard
+          <RouterLink :to="{name: 'sign'}" class="link link--is-simple">Sign in </RouterLink>
+        </p>
+      </article>
+    </Transition>
   </section>
 </template>
 <script setup lang="ts">
-  import BaseBadge from "@/app/ui/components/base/base-badge/BaseBadge.vue"
-  import { Is as IsBadge } from '@/app/ui/components/base/base-badge/types'
-  import { ChartBarIcon } from '@heroicons/vue/24/solid'
+import { storeToRefs } from "pinia";
+import { RouterLink } from "vue-router";
+import { computed } from "vue";
+
+// UI
+import BaseBadge from "@/app/ui/components/base/base-badge/BaseBadge.vue"
+import { Is as IsBadge } from '@/app/ui/components/base/base-badge/types'
+import { ChartBarIcon } from '@heroicons/vue/24/solid'
+
+// store
+import { userStore } from "@/domains/user/infrastructure/store/user";
+import { weatherStore } from "@/domains/weather/infrastructure/store/weather";
+
+const isUserLogged = storeToRefs(userStore).getUserLogged
+const hasWeatherList = storeToRefs(weatherStore).getWeatherList
+const isPageReady = computed(() => {
+  return [isUserLogged.value, hasWeatherList.value !== undefined].every(value => value === true)
+})
+
 </script>
 <style lang="scss" src="./Dashboard.scss" />
